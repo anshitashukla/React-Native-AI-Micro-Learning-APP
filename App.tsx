09387b44cbe06485f04cc8eb827fd5bf9ef27259
Anshita Shukla <46316966+anshitashukla@users.noreply.github.com>
2025-01-24 22:58:58 -0500

    Added More content to Home Screen (#2)
    * completed Main Layout
    
    * Home learning card complete
    
    * Home screen till Line divider
    
    ---------
    
    Co-authored-by: anshitashukla <anshita.shukla@gwu.edu>

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 // Import your screen types if they exist
-import {MainLayout} from './screens';
+import {MainLayout} from './src/screens';
 
 // Define the stack's parameter list for type safety
 type RootStackParamList = {
@@ -15,15 +16,17 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const App: React.FC = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-        initialRouteName="Dashboard">
-        <Stack.Screen name="Dashboard" component={MainLayout} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <GestureHandlerRootView>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+          initialRouteName="Dashboard">
+          <Stack.Screen name="Dashboard" component={MainLayout} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 };
 
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import IconButton from './IconButton';
+import TextButton from './TextButton';
+import VerticalCourseCard from './VerticalCourseCard';
+import IconLabel from './IconLabel';
+import LineDivider from './LineDivider';
+
+export {IconButton, TextButton, VerticalCourseCard, IconLabel, LineDivider};
